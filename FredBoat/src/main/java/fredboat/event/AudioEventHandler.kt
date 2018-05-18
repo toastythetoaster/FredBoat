664408e3f469f4034558515d827f934a7f92b8d8
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2018-05-18 13:37:04 +0200

    Voice state handling for LL

--- FredBoat/src/main/java/fredboat/event/AudioEventHandler.kt
+++ FredBoat/src/main/java/fredboat/event/AudioEventHandler.kt
@@ -9,19 +9,18 @@ import org.springframework.stereotype.Component
 @Component
 class AudioEventHandler(val lavalink: SentinelLavalink) : SentinelEventHandler() {
 
-    override fun onVoiceJoin(channel: VoiceChannel, member: Member) {
+    override fun onVoiceJoin(channel: VoiceChannel, member: Member) =
+            getLink(channel).setChannel(channel.id.toString())
 
-    }
+    override fun onVoiceLeave(channel: VoiceChannel, member: Member) =
+            getLink(channel).onDisconnected()
 
-    override fun onVoiceLeave(channel: VoiceChannel, member: Member) {
+    override fun onVoiceMove(oldChannel: VoiceChannel, newChannel: VoiceChannel, member: Member) =
+            getLink(newChannel).setChannel(newChannel.id.toString())
 
-    }
+    override fun onVoiceServerUpdate(voiceServerUpdate: VoiceServerUpdate) =
+            lavalink.onVoiceServerUpdate(voiceServerUpdate)
 
-    override fun onVoiceMove(oldChannel: VoiceChannel, newChannel: VoiceChannel, member: Member) {
-
-    }
-
-    override fun onVoiceServerUpdate(voiceServerUpdate: VoiceServerUpdate)
-            = lavalink.onVoiceServerUpdate(voiceServerUpdate)
+    private fun getLink(channel: VoiceChannel) = lavalink.getLink(channel.guildId.toString())
 
 }
