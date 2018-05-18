d30490801884b56cecb7ffd49b69a2fc23459354
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2018-05-18 13:18:36 +0200

    Derp

--- FredBoat/src/main/java/fredboat/audio/lavalink/SentinelLavalink.kt
+++ FredBoat/src/main/java/fredboat/audio/lavalink/SentinelLavalink.kt
@@ -3,8 +3,6 @@ package fredboat.audio.lavalink
 import fredboat.config.property.AppConfig
 import fredboat.sentinel.Sentinel
 import lavalink.client.io.Lavalink
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 
 @Service
@@ -25,20 +23,4 @@ class SentinelLavalink(
     }
 
     override fun buildNewLink(guildId: String) = SentinelLink(this, guildId)
-}
-
-interface JdaConfig {
-    var discordToken: String
-    var shardStart: Int
-    var shardEndExcl: Int
-    var shardCount: Int
-}
-
-@Component
-@ConfigurationProperties(prefix = "sentinel")
-class JdaConfigProperties : JdaConfig {
-    override var discordToken = ""
-    override var shardStart = 0
-    override var shardEndExcl = 1
-    override var shardCount = 1
 }
