fb4ee9c167828ae5cff14c9f6f6aa3031b80afd4
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2018-07-08 16:16:38 +0200

    Keep poking at sentinel till it responds

--- FredBoat/src/main/java/fredboat/agent/HelloSender.kt
+++ FredBoat/src/main/java/fredboat/agent/HelloSender.kt
@@ -3,8 +3,6 @@ package fredboat.agent
 import com.fredboat.sentinel.SentinelExchanges
 import com.fredboat.sentinel.entities.FredBoatHello
 import fredboat.config.property.AppConfig
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.stereotype.Service
 import java.util.concurrent.TimeUnit
@@ -15,18 +13,11 @@ class HelloSender(
         private val appConfig: AppConfig
 ) : FredBoatAgent("HelloSender", 5, TimeUnit.MINUTES) {
 
-    companion object {
-        private val log: Logger = LoggerFactory.getLogger(HelloSender::class.java)
-    }
-    private var firstRun = true
-
     override fun doRun() {
         // Send a hello when we start so we get SentinelHellos in return
         rabbitTemplate.convertAndSend(SentinelExchanges.FANOUT, "", FredBoatHello(
-                firstRun,
+                false,
                 appConfig.status
         ))
-        if (firstRun) log.info("Sent first hello to Sentinels")
-        firstRun = false
     }
 }
--- FredBoat/src/main/java/fredboat/sentinel/SentinelTracker.kt
+++ FredBoat/src/main/java/fredboat/sentinel/SentinelTracker.kt
@@ -1,23 +1,44 @@
 package fredboat.sentinel
 
+import com.fredboat.sentinel.SentinelExchanges
+import com.fredboat.sentinel.entities.FredBoatHello
 import com.fredboat.sentinel.entities.SentinelHello
 import fredboat.agent.FredBoatAgent
 import fredboat.agent.HelloSender
 import fredboat.config.property.AppConfig
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.stereotype.Service
 import java.util.concurrent.ConcurrentHashMap
 
 /** Class that tracks Sentinels and their routing keys */
 @Service
-class SentinelTracker(private val appConfig: AppConfig, helloSender: HelloSender) {
+class SentinelTracker(private val appConfig: AppConfig, rabbit: RabbitTemplate, helloSender: HelloSender) {
     companion object {
         private val log: Logger = LoggerFactory.getLogger(SentinelTracker::class.java)
     }
 
     init {
-        FredBoatAgent.startNow(helloSender)
+        fun hello(startup: Boolean) {
+            rabbit.convertAndSend(SentinelExchanges.FANOUT, "", FredBoatHello(startup, appConfig.status))
+        }
+
+        val task = launch {
+            log.info("Sending FredBoat hello")
+            hello(true)
+            delay(5000)
+            while (map.isEmpty()) {
+                log.info("Still haven't received hello from any sentinel. Resending hello....")
+                hello(false)
+                delay(5000)
+            }
+        }
+        task.invokeOnCompletion {
+            FredBoatAgent.start(helloSender)
+        }
     }
 
     /** Shard id mapped to [SentinelHello] */
