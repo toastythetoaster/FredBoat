524572da90048ffd81e9dfe1de038e823f48dbdf
Frederik Mikkelsen <fred@frederikam.com>
2018-12-07 14:42:41 +0100

    Report guild cache size to prom

--- FredBoat/src/main/java/fredboat/feature/metrics/collectors/FredBoatCollector.kt
+++ FredBoat/src/main/java/fredboat/feature/metrics/collectors/FredBoatCollector.kt
@@ -27,6 +27,7 @@ package fredboat.feature.metrics.collectors
 
 import fredboat.audio.lavalink.SentinelLavalink
 import fredboat.feature.metrics.BotMetrics
+import fredboat.sentinel.GuildCache
 import io.prometheus.client.Collector
 import io.prometheus.client.CounterMetricFamily
 import io.prometheus.client.GaugeMetricFamily
@@ -42,7 +43,8 @@ import java.util.*
 @Component
 class FredBoatCollector(
         private val botMetrics: BotMetrics,
-        private val ll: SentinelLavalink
+        private val ll: SentinelLavalink,
+        private val guildCache: GuildCache
 ) : Collector() {
 
     private var lastEntityCountHash = 0
@@ -65,6 +67,14 @@ class FredBoatCollector(
         val dockerPulls = CounterMetricFamily("fredboat_docker_pulls",
                 "Total fredboat docker image pulls as reported by the docker hub.", labelNames)
         mfs.add(dockerPulls)
+
+        val linkStateGauge = GaugeMetricFamily("fredboat_lavalink_link_states",
+                "Number of Lavalink Links in different states", listOf("state"))
+        mfs.add(linkStateGauge)
+
+        val guildCacheSize = GaugeMetricFamily("fredboat_guild_cache_size",
+                "Number of subscribed guilds", listOf("total"))
+        mfs.add(guildCacheSize)
 
         //global jda entity stats
         if (botMetrics.entityCounts != null && botMetrics.entityCounts?.hashCode() != lastEntityCountHash) {
@@ -94,14 +104,13 @@ class FredBoatCollector(
             dockerPulls.addMetric(Arrays.asList("total", "Db"), dockerStats.dockerPullsDb.toDouble())
         }
 
-        val linkStateGauge = GaugeMetricFamily("lavalink_link_states",
-                "Number of Lavalink Links in different states", listOf("state"))
         val statesMap = Link.State.values().associate { it to 0 }.toMutableMap()
         ll.links.forEach { statesMap[it.state] = statesMap[it.state]!! + 1 }
         statesMap.forEach { state, count ->
             linkStateGauge.addMetric(listOf(state.name), count.toDouble())
         }
-        mfs.add(linkStateGauge)
+
+        guildCacheSize.addMetric(listOf("total"), guildCache.cache.size.toDouble())
 
         return mfs
     }
